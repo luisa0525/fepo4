85e8b99da7859310a05beaab8e6ad038f96b9622
luisa <luisa0525ariza@gmail.com>
2023-11-01 17:50:15 -0500

    clase del miercoles 01112023

--- colores.py
+++ colores.py
@@ -51,3 +51,46 @@ for i in range(8):
     
 
 #como mostrar un solo punto de un solo color
+
+
+
+#      clase de 11-01--2023
+
+#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
+#git remote add origin https://github.com/luisa0525/fepo4.git
+a,b = 0,1
+
+while a<10:
+    print(a)
+    a,b = b,a+b
+    
+    
+def sumar(a,b):
+    c = a+b
+    return c
+print(sumar(3,8))
+
+def sumarTres(a):
+    x=a+3
+    return x
+y=sumarTres(7)+2
+print(y)
+
+
+x=int(input())
+y= int(input())
+z=x+y
+print(z)
+
+'''
+funcion1()
+
+x=int(input())
+y= int(input())
+return x+y
+'''
+
+
+def sumar(a,b):
+    return a+b
+
